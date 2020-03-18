350dfba35800e495494f88346bb1a75f4bc7ef64
matt johannsen <majohann@hotmail.com>
2020-03-18 13:42:41 -0500

    Added CustomersController and views to TrashCollector project

--- TrashCollector/TrashCollector/Data/ApplicationDbContext.cs
+++ TrashCollector/TrashCollector/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TrashCollector.Models;
 
 namespace TrashCollector.Data
 {
@@ -32,5 +33,6 @@ namespace TrashCollector.Data
 
                 );
         }
+        public DbSet<TrashCollector.Models.Customer> Customer { get; set; }
     }
 }
