8ee4564c33584dc6b562fcb9e91d1b387d4e0fcc
mattjohannsen <majohann@hotmail.com>
2020-03-19 07:50:03 -0500

    Changed DayOfWeek to PickUpDay to avoid problems with built in DateTime function.

---
+++ TrashCollector/TrashCollector/Data/Migrations/20200319124532_test.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector.Data.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customer_AspNetUsers_IdentityUserId",
+                table: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "DaysOfWeek");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customer_IdentityUserId",
+                table: "Customer");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "424ed1ff-fe8f-4427-99b2-3758aa663cfc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b05706ab-484c-4524-b230-5bef5670d9dd");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f317eb3e-6c20-4f20-a8f7-6bdede9e11ad");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "Customer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PickupDayId",
+                table: "Customer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "PickupDays",
+                columns: table => new
+                {
+                    PickupDayId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DayName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PickupDays", x => x.PickupDayId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "4df3adc5-43d3-48ce-b8b6-3475d1979463", "bc765ecc-06fe-4cad-8e5c-d7ae1374d691", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b2494363-78f5-4261-8c11-f7883c130a80", "7c169af6-ca09-4903-877f-e5f7c264060d", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "d2b8aaa6-5f05-4fab-a5e7-5eee0d3d4be4", "711a66ae-7ce3-4791-b218-38eb51dc2ec4", "Customer", "CUSTOMER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PickupDays");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4df3adc5-43d3-48ce-b8b6-3475d1979463");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b2494363-78f5-4261-8c11-f7883c130a80");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d2b8aaa6-5f05-4fab-a5e7-5eee0d3d4be4");
+
+            migrationBuilder.DropColumn(
+                name: "PickupDayId",
+                table: "Customer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId",
+                table: "Customer",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DaysOfWeek",
+                columns: table => new
+                {
+                    DaysOfWeekId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DayName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DaysOfWeek", x => x.DaysOfWeekId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b05706ab-484c-4524-b230-5bef5670d9dd", "572a4827-300a-45bd-9606-65ba974458f5", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f317eb3e-6c20-4f20-a8f7-6bdede9e11ad", "301061ee-7378-4e4a-9abe-3a23b17fbccf", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "424ed1ff-fe8f-4427-99b2-3758aa663cfc", "d62c804d-3fab-4e42-afe2-8d6a7bf55c0f", "Customer", "CUSTOMER" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_IdentityUserId",
+                table: "Customer",
+                column: "IdentityUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customer_AspNetUsers_IdentityUserId",
+                table: "Customer",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
